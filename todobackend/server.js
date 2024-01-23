ef3b47b813c62b22f22ba548350c35356d8c9ca3
g.thompson3141@gmail.com <g.thompson3141@gmail.com>
2024-01-23 18:34:05 +0000

    update

--- todobackend/server.js
+++ todobackend/server.js
@@ -2,17 +2,17 @@ const express = require("express");
 const errorHandler = require("./middleware/errorHandler");
 const connectDb = require("./config/dbConnection");
 const dotenv = require("dotenv").config();
-const cors = require('cors');
+const cors = require("cors");
 
 connectDb();
 
 const corsOptions = {
-    origin: 'http://localhost:3000', // Add the specific origin of your frontend
-    credentials: true,
+  origin: "http://localhost:3000", // Add the specific origin of your frontend
+  credentials: true,
 };
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 app.use(cors(corsOptions));
 app.use(express.json());
@@ -21,5 +21,5 @@ app.use("/api/users", require("./routes/userRoutes"));
 app.use(errorHandler);
 
 app.listen(port, () => {
-    console.log(`Server running on port: ${port}`);
+  console.log(`Server running on port: ${port}`);
 });
